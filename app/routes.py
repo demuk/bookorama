bc09966374fdf5393be590b621dcbf0544f12d2e
kiptoo <denysmutay@gmail.com>
2022-01-03 12:42:47 +0300

    updated view function to include the title

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return 'home'
-
+    title = 'Home'
+    return render_template('home.html', title=title)
