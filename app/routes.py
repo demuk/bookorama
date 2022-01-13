9e9746fc85cd4d641b25dd9f6c0fdd8fc1941894
kiptoo <denysmutay@gmail.com>
2022-01-14 00:17:05 +0300

    fixed an error which was not calling a positional arguement in the Login in FlaskForm

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app
 from flask import render_template
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/home')
@@ -12,6 +13,17 @@ def home():
 
 @app.route('/signup')
 def signup():
-    title = 'Sign Up'
+    title = 'Sign Up Form'
     return render_template('signup.html', title=title)
+
+
+
+# route for users login
+
+@app.route('/login')
+def login():
+    title = 'Login Form'
+    form = LoginForm()
+    return render_template('login.html', title=title, form=form)
+
 
