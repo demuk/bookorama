b4967c9ef0e2acdec91d082d37da8521c0e40f6b
kiptoo <denysmutay@gmail.com>
2022-01-24 06:26:12 +0300

    added methods GET and POST

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
+from crypt import methods
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect
 from app.forms import LoginForm
 
 @app.route('/')
@@ -20,10 +21,14 @@ def signup():
 
 # route for users login
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'] )
 def login():
     title = 'Login Form'
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'
+            .format(form.username.data, form.remember_me.data))
+        return redirect('/home')
     return render_template('login.html', title=title, form=form)
 
 
