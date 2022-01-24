babd7dbbc71aa76829726a1162d888f41b543399
kiptoo <denysmutay@gmail.com>
2022-01-24 06:57:17 +0300

    added url_for() to generates URLs using its internal mapping of URLs to view functions

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from crypt import methods
 from app import app
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
 
 @app.route('/')
@@ -28,7 +28,7 @@ def login():
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'
             .format(form.username.data, form.remember_me.data))
-        return redirect('/home')
+        return redirect(url_for('home'))
     return render_template('login.html', title=title, form=form)
 
 
