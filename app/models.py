0b66a304137350d9f41bd8b2c16485f294dfba81
kiptoo <denysmutay@gmail.com>
2022-01-26 20:25:28 +0300

    added one to many relationship between users tables and the books table, where a user can have many books

--- app/models.py
+++ app/models.py
@@ -6,6 +6,7 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    books = db.relationship('Book', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -17,4 +18,9 @@ class Book(db.Model):
     author = db.Column(db.String(140))
     genre = db.Column(db.String(56))
     timestamp = db.Column(db.DateTime, index=True, unique=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return 'Book {}>'.format(self.body)
+
     
